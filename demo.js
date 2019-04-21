42a88c8d8a51dfdc5eb36eefae736a313291f736
罗培中 <lpz2@163.com>
2019-04-21 11:27:53 +0800

    SOLVED::js类型

--- demo.js
+++ demo.js
@@ -1,5 +1,19 @@
 // alert("再欢迎");
 // 弱类型
+/* 
 var box = 'lpz';
 box = 100;
-alert(box);
+alert(box); 
+*/
+
+var box = 'lpz';
+// typeof 是 + - * / 操作运算符
+alert(typeof box);
+
+function test() {
+    console.log('aaa');
+}
+alert(typeof test);
+
+var o = new Object;
+alert(typeof o);
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # javascript学习例子收集
 
 ## js是弱类型语言,变量直接赋值,赋值的类型决定变量的类型
+
+## js变量类型 5种简单类型 Undefined  Null  Boolean Number  String   复杂类型 Object
+*函数是对象，不是一种数据类型, 是Object类型的一个实例？*
+*typeof 返回的都是对象类型描述字符串*
